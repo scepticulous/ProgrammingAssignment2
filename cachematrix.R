17378a712e7ec09ed3c9a1760c5da2b369eeddde
OMCnet Development Team <technik@omc.net>
2015-02-18 11:58:35 +0100

    Implemented makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(arg) inverse <<- arg
+  get_inverse <- function() inverse
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get_inverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  matrix_ <- x$get()
+  inverse <- solve(matrix_, ...)
+  x$set_inverse(inverse)
+  inverse
 }
