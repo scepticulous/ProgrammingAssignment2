35c3d96d9648c41c255adaffe0c04454b1d8bb32
scepticulous <dev@d-coded.de>
2015-02-19 21:52:19 +0100

    added proper documentation to the file and all functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module implements a cached version of the matrix inverxe operation
+## by storing the result in the current environment
 
+# create a list of a matrix and some operations that will serve
+# as some kind of a smart-matrix that supports caching the inverse operation
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
   set <- function(y) {
@@ -15,6 +17,9 @@ makeCacheMatrix <- function(x = matrix()) {
        get_inverse = get_inverse)
 }
 
+# caculate the inverse of a matrix.
+# this function tries to retrieve a cached value
+# and only calculates the inverse if that value is not present yet
 
 cacheSolve <- function(x, ...) {
   inverse <- x$get_inverse()
